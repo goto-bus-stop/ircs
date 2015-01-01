dd8e7325442905d139ebfd4f80c6db807af23ad2
René Kooi <rene@kooi.me>
2015-01-01 22:50:29 +0100

    add /WHOIS

--- src/Commands.js
+++ src/Commands.js
@@ -150,7 +150,23 @@ Commands.prototype.PRIVMSG = function (user, targetName, content) {
   }
 
   if (!target) {
-    var ERR_NOSUCHNICK = '401'
     user.send(this.server.mask(), r.ERR_NOSUCHNICK, [ user.nickname, targetName, 'No such nick/channel' ])
+  }
+}
+
+/**
+ * IRC /WHOIS command.
+ */
+Commands.prototype.WHOIS = function (user, nickmask) {
+  var target = this.server.findUser(nickmask)
+  var mask = this.server.mask()
+  if (target) {
+    user.send(mask, r.RPL_WHOISUSER, [ user.nickname, target.username, target.hostname, '*', user.realname ])
+    user.send(mask, r.RPL_WHOISSERVER, [ user.nickname, target.username, target.servername, '' ])
+    user.send(mask, r.RPL_ENDOFWHOIS, [ user.nickname, target.username, 'End of /WHOIS list.' ])
+  }
+  else {
+    user.send(mask, r.ERR_NOSUCHNICK, [ user.nickname, nickmask, 'No such nick/channel.' ])
+    user.send(mask, r.RPL_ENDOFWHOIS, [ user.nickname, nickmask, 'End of /WHOIS list.' ])
   }
 }
