0840fc38a546b76c33e69ef3bebc864de26bc321
René Kooi <rene@kooi.me>
2015-04-02 22:02:51 +0200

    jscs checks!

--- src/defaultCommands.js
+++ src/defaultCommands.js
@@ -114,8 +114,8 @@ export default function (ircs) {
     if (channel) {
       let mask = ircs.mask()
       channel.users.forEach(u => {
-        user.send(mask, r.RPL_WHOREPLY, [ user.nickname, channel.name, u.username, u.hostname,
-                                          u.servername, u.nickname, 'H', ':0', u.realname ])
+        user.send(mask, r.RPL_WHOREPLY, [ user.nickname, channel.name, u.username, u.hostname
+                                        , u.servername, u.nickname, 'H', ':0', u.realname ])
       })
       user.send(mask, r.RPL_ENDOFWHO, [ user.nickname, channelName, 'End of /WHO list.' ])
     }
