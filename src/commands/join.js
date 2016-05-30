a75d08b1dc94792e58bc62a2a1ab8c2007aa5974
René Kooi <renee@kooi.me>
2016-05-30 16:27:40 +0200

    /JOIN: fix crash when no channel is given

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,15 +1,21 @@
 import {
   RPL_TOPIC,
-  RPL_NOTOPIC
+  RPL_NOTOPIC,
+  ERR_NEEDMOREPARAMS
 } from '../replies'
 
 import names from './names'
 
 export default function join (opts) {
   const { user, server, parameters: [ channelName ] } = opts
+  const mask = server.mask()
 
-  let channel = server.getChannel(channelName)
-  let mask = server.mask()
+  if (!channelName) {
+    user.send(mask, ERR_NEEDMOREPARAMS, [ 'JOIN', 'Not enough parameters' ])
+    return
+  }
+
+  const channel = server.getChannel(channelName)
   channel.join(user)
 
   channel.send(user.mask(), 'JOIN', [ channel.name, user.username, user.realname ])
