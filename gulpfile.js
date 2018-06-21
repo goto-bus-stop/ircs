0bdc762ad42c83db39a1a722cd001871f3e1e301
Renée Kooi <renee@kooi.me>
2018-06-21 10:54:58 +0200

    Make a bundle

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+var fs = require('fs')
 var gulp = require('gulp')
 var babel = require('gulp-babel')
 var newer = require('gulp-newer')
@@ -6,11 +7,12 @@ var through = require('through2')
 var log = require('gulp-util').log
 var colors = require('gulp-util').colors
 var relative = require('path').relative
+var browserify = require('browserify')
 
 var src = 'src/**/*.js'
 var dest = 'lib/'
 
-function build () {
+function transpile () {
   return gulp.src(src)
     .pipe(plumber())
     .pipe(newer(dest))
@@ -22,6 +24,15 @@ function build () {
     .pipe(babel())
     .pipe(gulp.dest(dest))
 }
+
+function bundle () {
+  return browserify('./lib/index.js', { node: true })
+    .plugin('browser-pack-flat')
+    .bundle()
+    .pipe(fs.createWriteStream('./lib/ircs.js'))
+}
+
+var build = gulp.series(transpile, bundle)
 
 function watch () {
   gulp.watch(src, build)
